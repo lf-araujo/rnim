4e98e7fc35c24de346c63e8bbeba1e19758de092
lf-araujo <ldearaujo@unimelb.edu.au>
2023-03-12 14:52:42 -0400

    Added automatic path detection in correction. Needs testing.

--- R/loadNim.R
+++ R/loadNim.R
@@ -1,7 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+# loadNim
 #
 # You can learn more about package authoring with RStudio at:
 #
@@ -12,17 +9,15 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
-
 #' Loads a Nim library file to R memory
 #'
 #' @param name Name of library file
 #' @param path Local path to nim
 loadNim <- function(name, path) {
-  if (missing(path))  path = "~/.nimble/bin/"
+  if (missing(path)) path <- "~/.nimble/bin/"
   Sys.setenv(PATH = paste0(path, Sys.getenv("PATH")))
-  nim.installed <- invisible(system(paste0(path,'nim -v')))==0
-  if (!nim.installed){
+  nim.installed <- invisible(system(paste0(path, "nim -v"))) == 0
+  if (!nim.installed) {
     message("You havent installed Nim! Please follow installation instructions from https://nim-lang.org/")
     stop()
   }
@@ -31,6 +26,8 @@ loadNim <- function(name, path) {
   #   message("You don't have rnim installed, please run nimble install rnim")
   #   stop()
   # }
-  system(paste0(path,"nim c --app:lib ", name))
-  dyn.load(paste0("lib",name,".so"))
+  if (Sys.getenv("R_HOME")=="") Sys.setenv(R_HOME = file.path(R.home("bin"), "R"))
+  if (Sys.getenv("LD_LIBRARY_PATH")=="") Sys.setenv(LD_LIBRARY_PATH = file.path(R.home()))
+  system(paste0(path, "nim c --app:lib ", name))
+  dyn.load(paste0("lib", name, ".so"))
 }
